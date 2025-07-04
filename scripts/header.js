2605f9a26614cef85555421b0f296078c25efb86
garba-js <joegarba98@gmail.com>
2025-07-05 00:01:33 +0100

    Update

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,10 @@
 const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
+ 
 });
 
 // ✅ Display Current Year and Last Modified Date
